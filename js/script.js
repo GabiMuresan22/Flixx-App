28b39ac5b58c43b3344e8547ec822cc42eafa1ba
Gabi <gabimuresan2289@gmail.com>
2023-11-04 21:10:59 +0100

    create function to add backdrop image

--- js/script.js
+++ js/script.js
@@ -86,6 +86,10 @@ async function displayMovieDetails() {
 
   const div = document.createElement('div');
 
+  // Overlay for background image 
+
+  displayBackgroundImage('movie', movie.backdrop_path);
+
   div.innerHTML = ` <div class="details-top">
   <div>
   ${
@@ -133,6 +137,30 @@ async function displayMovieDetails() {
 </div>`;
 
 document.querySelector('#movie-details').appendChild(div);
+}
+
+// Display Backdrop on Details Pages form Movie and Show
+
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+
+  if(type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
 }
 
 // Fetch data from TMDb API
