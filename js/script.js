ad4f7fe10bf91c6f335cf12657045cb96198a697
Gabi <gabimuresan2289@gmail.com>
2023-11-21 22:34:36 +0100

    update show link in show file

--- js/script.js
+++ js/script.js
@@ -501,7 +501,7 @@ function init() {
       displaySlider();
       displayPopularMovies();
       break;
-    case 'shows.html':
+    case 'shows':
       displayPopularShows();
       break;
     case '/movie-details.html':
